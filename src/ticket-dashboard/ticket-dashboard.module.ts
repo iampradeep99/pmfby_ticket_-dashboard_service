3ffab744183a1825f73d44515cb09ff035de81d5
iampradeep99 <pradeep.meandev@gmail.com>
2025-08-30 12:12:52 +0530

    new change

--- src/ticket-dashboard/ticket-dashboard.module.ts
+++ src/ticket-dashboard/ticket-dashboard.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TicketDashboardController } from './ticket-dashboard.controller';
 import { TicketDashboardService } from './ticket-dashboard.service';
 import { DatabaseModule } from '../database/database.module';
-
+import { RedisModule } from '../commonServices/redis.module';
+import { UtilModule } from 'src/commonServices/util.module';
 @Module({
-  imports:[DatabaseModule],
+  imports:[DatabaseModule,RedisModule,UtilModule],
   controllers: [TicketDashboardController],
   providers: [TicketDashboardService]
 })
