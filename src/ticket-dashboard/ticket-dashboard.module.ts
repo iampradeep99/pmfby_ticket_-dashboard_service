05960ae7ca8003415dbe095dcd75d3cf3c7718ea
iampradeep99 <pradeep.meandev@gmail.com>
2025-09-11 13:06:12 +0530

    new change

--- src/ticket-dashboard/ticket-dashboard.module.ts
+++ src/ticket-dashboard/ticket-dashboard.module.ts
@@ -5,7 +5,7 @@ import { DatabaseModule } from '../database/database.module';
 import { RedisModule } from '../commonServices/redis.module';
 import { UtilModule } from 'src/commonServices/util.module';
 import { MailModule } from 'src/mail/mail.module';
-import { RabbitMQModule } from 'src/commonServices/rabbitmq/rabbitmq.module';
+import { RabbitMQModule } from 'src/commonServices/rabbitMQ/rabbitmq.module';
 
 @Module({
   imports: [
