1cc42ca1ac481f3b14253928d6edaff154f0c89b
iampradeep99 <pradeep.meandev@gmail.com>
2025-09-11 23:20:09 +0530

    new changre

--- src/ticket-dashboard/ticket-dashboard.service.ts
+++ src/ticket-dashboard/ticket-dashboard.service.ts
@@ -3807,17 +3807,7 @@ async farmerCallingHistoryDownloadReportAndZip(payload: any) {
   }
 }
 
-async formatTimestamp(timestamp: string): Promise<string> {
-  const date = new Date(timestamp);
 
-  const day = String(date.getUTCDate()).padStart(2, '0');
-  const month = String(date.getUTCMonth() + 1).padStart(2, '0');
-  const year = date.getUTCFullYear();
-  const hours = String(date.getUTCHours()).padStart(2, '0');
-  const minutes = String(date.getUTCMinutes()).padStart(2, '0');
-
-  return `${day}-${month}-${year} ${hours}:${minutes}`;
-}
 
 
 
