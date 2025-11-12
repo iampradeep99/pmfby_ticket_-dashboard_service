7737d58631122fa7934e34f7c735dfdb100c47a6
Pradeep Sahani <pradeep.meandev@gmail.com>
2025-11-12 13:28:24 +0530

    new chnage in the code

--- src/database/mongo.provider.ts
+++ src/database/mongo.provider.ts
@@ -1,19 +1,3 @@
-// import { MongoClient, Db } from 'mongodb';
-// import { Provider } from '@nestjs/common';
-// import { ConfigService } from '@nestjs/config';
-
-// export const DATABASE_NAME = 'krph_db';
-
-// export const MongoProvider: Provider = {
-//   provide: 'MONGO_DB',
-//   inject: [ConfigService],
-//   useFactory: async (configService: ConfigService): Promise<Db> => {
-//     const uri = configService.get<string>('MONGODBPRODURL');
-//     const client = new MongoClient(uri);
-//     await client.connect();
-//     return client.db(DATABASE_NAME);
-//   },
-// };
 
 
 import { MongoClient, Db } from 'mongodb';
