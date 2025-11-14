a2235a68e67f00f04e578ad0c94ab2d35f316f46
Pradeep Sahani <pradeep.meandev@gmail.com>
2025-11-14 18:19:48 +0530

    SLA Converion code

---
+++ src/commonServices/holidays.ts
@@ -0,0 +1,65 @@
+// holidays.ts
+
+export const fixedHolidays = {
+  "01-26": "Republic Day",
+  "08-15": "Independence Day",
+  "10-02": "Gandhi Jayanti",
+};
+
+export const floatingHolidays = {
+  2023: {
+    "03-08": "Holi",
+    "04-22": "Eid al-Fitr",
+    "11-12": "Diwali",
+  },
+  2024: {
+    "03-25": "Holi",
+    "04-10": "Eid al-Fitr",
+    "11-01": "Diwali",
+  },
+  2025: {
+    "03-14": "Holi",
+    "03-30": "Eid al-Fitr",
+    "10-20": "Diwali",
+  },
+  2026: {
+    "03-04": "Holi",
+    "04-06": "Eid al-Fitr",
+    "11-10": "Diwali",
+  },
+  2027: {
+    "03-23": "Holi",
+    "05-01": "Eid al-Fitr",
+    "11-01": "Diwali",
+  },
+  2028: {
+    "03-13": "Holi",
+    "04-10": "Eid al-Fitr",
+    "10-25": "Diwali",
+  },
+  2029: {
+    "03-01": "Holi",
+    "04-22": "Eid al-Fitr",
+    "11-02": "Diwali",
+  },
+  2030: {
+    "03-20": "Holi",
+    "05-01": "Eid al-Fitr",
+    "11-12": "Diwali",
+  },
+  2031: {
+    "03-10": "Holi",
+    "04-21": "Eid al-Fitr",
+    "10-13": "Diwali",
+  },
+  2032: {
+    "03-29": "Holi",
+    "05-10": "Eid al-Fitr",
+    "11-03": "Diwali",
+  },
+  2033: {
+    "03-18": "Holi",
+    "04-10": "Eid al-Fitr",
+    "11-06": "Diwali",
+  },
+};
