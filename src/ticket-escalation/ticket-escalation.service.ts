deeb37486a315a75fe008542ca1d189437e9f483
Pradeep Sahani <pradeep.meandev@gmail.com>
2025-12-30 19:31:37 +0530

    ss

--- src/ticket-escalation/ticket-escalation.service.ts
+++ src/ticket-escalation/ticket-escalation.service.ts
@@ -2722,7 +2722,7 @@ Status              : ${syncedCount > 0 && failedCount > 0 ? "PARTIAL" : failedC
 
 
       const [stateAssigned] = await Promise.all([
-        this.db.collection("Ticket_Assignment_History").aggregate(statePipeline).toArray(),
+        this.db.collection("Ticket_Assignment").aggregate(statePipeline).toArray(),
       ]);
 
       if (stateAssigned.length === 0) {
