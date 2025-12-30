07431f175b89f977de6a68124788de1c727bcffe
Pradeep Sahani <pradeep.meandev@gmail.com>
2025-12-30 18:33:45 +0530

    ss

--- src/ticket-escalation/ticket-escalation.service.ts
+++ src/ticket-escalation/ticket-escalation.service.ts
@@ -1063,11 +1063,13 @@ async getToken() {
           InsuranceCompanyId: ticket?.InsuranceCompanyID,
           InsuranceCompanyName: ticket?.InsuranceCompany,
           TicketComment:ticketDescription
+
         };
 
-        await assignHistoryCollection.insertOne(assignmentData)
-
-      
+       let insertedRecords =  await assignHistoryCollection.insertOne({
+          ...assignmentData,
+          CreatedDate: now
+        });
       
 
         await currentAssignCollection.updateOne(
@@ -1288,7 +1290,7 @@ async getToken() {
       const toDateISO = toDate ? moment(toDate, "YYYY-MM-DD").endOf("day").toDate() : null;
 
       const pipeline: any[] = [
-        { $match: { assignedTo: loggedInUserId, AssignedDistrictID:{$eq:""} } },
+        { $match: { assignedTo: loggedInUserId } },
         {
           $lookup: {
             from: "SLA_Ticket_listing",
@@ -1514,8 +1516,6 @@ async getToken() {
         },
       ];
 
-
-      console.log(JSON.stringify(pipeline))
       const data = await collection.aggregate(pipeline).toArray();
 
       return {
