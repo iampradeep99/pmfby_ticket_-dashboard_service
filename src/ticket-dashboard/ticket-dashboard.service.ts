661eb3219a5998bd039281f5c92c9c87fbd0aab3
Pradeep Sahani <pradeep.meandev@gmail.com>
2026-01-06 10:19:32 +0530

    ss

--- src/ticket-dashboard/ticket-dashboard.service.ts
+++ src/ticket-dashboard/ticket-dashboard.service.ts
@@ -1078,7 +1078,7 @@ export class TicketDashboardService {
     ]
 
     // const countResult = await db.collection("SLA_KRPH_SupportTickets_Records").aggregate(countPipeline).toArray()
-    const countResult = await db.collection("SLA_Ticket_listing").aggregate(countPipeline).toArray()
+    const countResult = await db.collection("SLA_KRPH_SupportTickets_Records").aggregate(countPipeline).toArray()
 
     const totalCount = countResult?.[0]?.total || 0
     const totalPages = Math.ceil(totalCount / limit)
