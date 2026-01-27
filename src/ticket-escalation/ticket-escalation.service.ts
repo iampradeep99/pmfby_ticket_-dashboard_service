3191afcb972e2c9cc05b5d9899b0b04fe9153828
Pradeep Sahani <pradeep.meandev@gmail.com>
2026-01-27 17:10:19 +0530

    new change in the code

--- src/ticket-escalation/ticket-escalation.service.ts
+++ src/ticket-escalation/ticket-escalation.service.ts
@@ -40,8 +40,8 @@ export class TicketEscalationService {
   private tokenCache = new NodeCache({ stdTTL: 30 * 60 });
 
   private readonly PMFBY_ROLE_URL = config.pmfbyRoleURL
-  // private readonly RoleURL = "https://pmfby.gov.in/krishirakshak/v1/user/user/roleWiseUserList"
-   private readonly RoleURL = "http://10.128.60.9:3011/krishirakshak/v1/user/user/roleWiseUserList"
+  private readonly RoleURL = "https://pmfby.gov.in/krishirakshak/v1/user/user/roleWiseUserList"
+  //  private readonly RoleURL = "http://10.128.60.9:3011/krishirakshak/v1/user/user/roleWiseUserList"
 
   public gcp = new GCPServices();
   logDir = path.join(__dirname, '..', 'logs');
