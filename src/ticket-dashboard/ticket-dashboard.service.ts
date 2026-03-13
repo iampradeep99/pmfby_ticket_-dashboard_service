817abf0438d8c8f218274e6c623b8a814d285848
Your Name <you@example.com>
2026-03-13 14:10:13 +0530

    chnaged in the code

--- src/ticket-dashboard/ticket-dashboard.service.ts
+++ src/ticket-dashboard/ticket-dashboard.service.ts
@@ -7366,7 +7366,7 @@ export class TicketDashboardService {
                   AgentName: 1,
                   CreatedBY: 1,
                   CallingUserID: 1,
-                  IsAssignedTicket:1,
+                  IsAssignedTicket: { $ifNull: ["$IsAssignedTicket", null] },
                   TicketReOpenDate: {
                     $cond: {
                       if: { $or: [{ $eq: ["$TicketReOpenDate", null] }, { $eq: ["$TicketReOpenDate", ""] }] },
